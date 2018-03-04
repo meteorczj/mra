88045ecbf4cb461aa5f1260b16abb6277b6926fa
zhujun chu <meteorczj@qq.com>
2018-03-04 20:37:31 +0800

    Test.java增加打印测试4
    Signed-off-by: zhujun chu <meteorczj@qq.com>

--- src/Test.java
+++ src/Test.java
@@ -6,7 +6,7 @@ public class Test {
 	public static void main(String[] args) {
 		Date date = new Date(2099,01,01);
 		Long time = date.getTime();
-		System.out.println("time3:" + time);
+		System.out.println("time4:" + time);
 	}
 
 }
